cc158f423e97a810b0ccee0e1cec34d2f80aaa7d
Manoel Dias <115195961+ManoeldeCastro@users.noreply.github.com>
2022-11-25 13:51:06 -0300

    Create obj-Dinamico.js

---
+++ objetos/obj-Dinamico.js
@@ -0,0 +1,4 @@
+let print = "log"
+
+console[print]("imprimindo")
+// podemos usar os objetos de forma dinâmica, como o console é um objeto que existe na biblioteca do JS. usamos o método "log" de forma dinâmica.
