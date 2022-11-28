741f5bf506958cb318233d5457c70bb48bf11208
Manoel Dias <115195961+ManoeldeCastro@users.noreply.github.com>
2022-11-28 20:07:55 -0300

    Create sort.js

---
+++ array/sort.js
@@ -0,0 +1,16 @@
+const personagens = [
+  { nivel: 42, nome: "Thrall", raca: "Orc", classe: "Xamã" },
+  { nivel: 28, nome: "Garrosh", raca: "Orc", classe: "Guerreiro" },
+  { nivel: 35, nome: "Varok", raca: "Orc", classe: "Guerreiro" },
+  { nivel: 35, nome: "Uther", raca: "Humano", classe: "Paladino" },
+  { nivel: 26, nome: "Jaina", raca: "Humano", classe: "Maga" },
+  { nivel: 39, nome: "Tyrande", raca: "Elfo Noturno", classe: "Sacerdotisa" },
+  { nivel: 29, nome: "Muradin", raca: "Anão", classe: "Guerreiro" },
+]
+
+personagens.sort((a, b) => {
+  return b.nivel - a.nivel
+})
+
+console.log(personagens)
+// método sort ordena os elementos em ordem, os parâmetros acima representa os elementos, nesse caso estou acessando em cada elemento a propriedade .nivel e organizando em ordem crescente se o resultado a operação de returno for positivo ele faz em ordem crescente, caso seja negativo em ordem decrescente.
