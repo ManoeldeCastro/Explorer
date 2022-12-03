16d8b92fdd0e2055306c14fffa4dc710bc2c7abc
Manoel Dias <115195961+ManoeldeCastro@users.noreply.github.com>
2022-12-03 10:05:37 -0300

    COMMIT

---
+++ DOM/Eventos/JS/events.js
@@ -0,0 +1,15 @@
+function clickMe() {
+  console.log("Fui CLicado!")
+}
+function focusMe() {
+  console.log("Fui focado!")
+}
+function blurMe() {
+  console.log("Fui desfocado!")
+}
+function changeMe() {
+  console.log("Fui alterado!")
+}
+function overMe() {
+  console.log("Passou o mouse sobre mim!")
+}
