5ed94434346f98a1415e9e841cbfba5fa17e5cb3
manoel <manoeldiasmf@outlook.com>
2022-12-07 13:38:44 -0300

    Module

---
+++ ES6+/6 - Modulo/index.js
@@ -0,0 +1,6 @@
+import Spaceship from "./spaceship.js"
+import armamentKind from "./armaments.js"
+
+const spaceships = new Spaceship("USS Enterprise", "James", armamentKind.laser)
+
+console.log(spaceships)
---
+++ ES6+/6 - Modulo/spaceship.js
@@ -0,0 +1,9 @@
+class Spaceship {
+  constructor(name, captain, armaments = [], defenses = []) {
+    this.name = name
+    this.captain = captain
+    this.armaments = armaments
+    this.defenses = defenses
+  }
+}
+export default Spaceship
