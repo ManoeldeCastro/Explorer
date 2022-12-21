8ba3e0abad81260074fa39ad4584317c2e88568d
manoel <manoeldiasmf@outlook.com>
2022-12-21 20:53:21 -0300

    Update server.js

--- Rocket/Stage 07 - Node.Js/API/src/server.js
+++ Rocket/Stage 07 - Node.Js/API/src/server.js
@@ -2,10 +2,11 @@ const express = require("express")
 
 const app = express()
 
-const PORT = 3333
+const PORT = 3334
 
-app.get("/message", (request, response) => {
-  response.send("Hello, world!")
+app.get("/message/:id/:user", (request, response) => {
+  response.send(`Id da mensagem: ${request.params.id}
+                User: ${request.params.user}`)
 })
 
 app.listen(PORT, () => console.log(`Server is running at ${PORT}`))
